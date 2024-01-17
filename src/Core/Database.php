086416695e61bdcf4cbc582a4262b5a5c6b10e7a
tunim73 <antonio.netto973@gmail.com>
2024-01-17 04:03:13 -0300

    update Database.php remove options

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -14,9 +14,9 @@ class Database
         $base = getenv('DB_BASE');
 
         $dsn = "mysql:host=$host;dbname=$base;charset=UTF8;";
-        $options = [PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt"];
+        /*$options = [PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "../../ca-certificates.crt" ];*/
 
-        $pdo = new PDO($dsn, $user, $pass, $options);
+        $pdo = new PDO($dsn, $user, $pass);
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -33,3 +33,13 @@ $base = $_ENV['DB_BASE'];*/
 DB_USER=laranja
 DB_PASSWORD=mynewpassword
 DB_BASE=api_php_puro*/
+
+/*DB_HOST=aws-sa-east-1.connect.psdb.cloud
+DB_USER=wyqppa14zdm9qek3hc49
+DB_PASSWORD=pscale_pw_hAanvR9GD6GoDCGwkeJPxNKyuxrCIIWYMuA2Bpj7ktd
+DB_BASE=api_php_puro
+PLANETSCALE_DB
+PLANETSCALE_DB_USERNAME
+PLANETSCALE_DB_PASSWORD
+PLANETSCALE_DB_HOST
+PLANETSCALE_SSL_CERT_PATH*/
